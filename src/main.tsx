734179d5420d3f957f3e7cbe53187a07bde37580
Stein-B06 <Steinb06.kodehode@gmail.com>
2023-11-13 18:39:02 +0100

    updated import and path in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import BeerTypes from "./routes/beertypes";
 import ErrorPage from "./routes/errorpage";
-import AboutPage from "./routes/aboutpage";
-
+import Beerhistory from "./routes/DataFetcher";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainPage from "./routes/mainpage";
 const router = createBrowserRouter([
@@ -23,8 +22,8 @@ const router = createBrowserRouter([
         element: <MainPage />,
       },
       {
-        path: "/about",
-        element: <AboutPage />,
+        path: "/history",
+        element: <Beerhistory />,
       },
     ],
   },
